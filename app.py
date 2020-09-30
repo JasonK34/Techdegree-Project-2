09196b6dba8ceae44b5b2f375568ff9b98bfb18d
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-09-30 16:45:26 -0700

    changed location of team lists
    Placed lists above functions

--- app.py
+++ app.py
@@ -7,40 +7,44 @@ import copy
 the_players = copy.deepcopy(PLAYERS)
 the_teams = copy.deepcopy(TEAMS)
 
+
+panthers = the_teams[0]
+panthers_players = []
+
+bandits = the_teams[1] 
+bandits_players = []
+
+warriors = the_teams[2]
+warriors_players = []
+
 print("\nWelcome to Basketball Stats!")
 
+
 def clean_data():
-    #print(the_players)
     print(len(the_players))
 
+
 def balance_teams():
-    panthers = the_teams[0]
-    panthers_players = []
+
     panthers_players.append(the_players[0:6])
-
-    bandits = the_teams[1] 
-    bandits_players = []
     bandits_players.append(the_players[6:13])
-
-    warriors = the_teams[2]
-    warriors_players = []
     warriors_players.append(the_players[13:])
 
-    #print(the_teams)
-    #print(panthers, bandits, warriors)
-    #print(f"Your Panthers are: {panthers_players}, \n\nYour Bandits are: {bandits_players}, \n\nYour Warriors are: {warriors_players}")
-
+   
 def stat_tool():
     balance_teams()
     initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n"))
     if initial_entry == 1:
         select_team = int(input("Select team (enter numer): (1) Panthers, (2) Bandits, (3) Warriors: \n"))
         if select_team == 1:
-            balance_teams()
+            #balance_teams()
             print(panthers, ": ", panthers_players) 
-        #clean_data()
-        #balance_teams()
-        
+        if select_team == 2:
+            #balance_teams()
+            print(bandits, ": ", bandits_players)
+        if select_team == 3:
+            #balance_teams()
+            print(warriors, ": ", warriors_players)
     
 
 if __name__ == "__main__":
