a432ebaf9a3f72813480edad8040acd287e5fc86
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-09-29 22:31:51 -0700

    setting up teams and adding players

--- app.py
+++ app.py
@@ -10,10 +10,25 @@ the_teams = copy.deepcopy(TEAMS)
 print("\nWelcome to Basketball Stats!")
 
 def clean_data():
-    print(the_players)
+    #print(the_players)
+    print(len(the_players))
 
 def balance_teams():
+    panthers = the_teams[0]
+    panthers_players = []
+    panthers_players.append(the_players[0:6])
+
+    bandits = the_teams[1] 
+    bandits_players = []
+    bandits_players.append(the_players[6:13])
+
+    warriors = the_teams[2]
+    warriors_players = []
+    warriors_players.append(the_players[13:])
+
     print(the_teams)
+    print(panthers, bandits, warriors)
+    print(f"Your Panthers are: {panthers_players}, \n\nYour Bandits are: {bandits_players}, \n\nYour Warriors are: {warriors_players}")
 
 def stat_tool():
     initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n"))
