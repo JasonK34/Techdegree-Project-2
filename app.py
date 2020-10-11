32a0dacfd25e641cc04acce3684f0fd45aa3ac00
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-10-11 16:00:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -29,51 +29,50 @@ def clean_data():
 
             if key == "guardians":
                 guardian_list.append(value)
-            
+                for guardian in guardian_list:
+                    if "and" in guardian:
+                        guardian.pop("and")
+                print(guardian_list) 
+
             if key == "height":
                 num_inches = value.split(" ")
                 num = int(num_inches[0])
                 #print (num)
-
-            #if players_copy['experience'] = "YES":
-               # YES = True
                 
-
-
-    balance_teams()
+ 
     
+def balance_teams():
     
-    initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n"))
+    initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n\n ---> "))
     while initial_entry == 1:
-        select_team = int(input("\nSelect option below (enter numer):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) ALL Players\n (5) All Guardians\n"))
+        select_team = int(input("\nSelect option below (enter numer):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) ALL Players\n (5) All Guardians\n\n ---> "))
         if select_team == 1:
             print("\nPANTHERS:")
-            for panther in players_copy:
-                print(panther)
-                if select_team == 2:
+            for player in players_copy[0:6]:
+                for key, value in player.items():
+                    if key == "name":
+                        print(value)
+
+        if select_team == 2:
             print("\nBANDITS: ")
-            for bandit in bandits_players:
-                print(bandit)
+            for player in players_copy[6:12]:
+                for key, value in player.items():
+                    if key == "name":
+                        print(value)
         if select_team == 3:
             print("\nWARRIORS: ")
-            for warrior in warriors_players:
-                print(warrior)
+            for player in players_copy[12:]:
+                for key, value in player.items():
+                    if key == "name":
+                        print(value)
         if select_team == 4:
-            print("\nALL players: \n")
+            print("ALL players: \n")
             for player in players_list:
                 print(player)
         if select_team == 5:
             print("\nGUARDIANS: \n")
             for guardian in guardian_list:
                 print(guardian)    
-
-def balance_teams():
-    
-    panthers_players.append(players_list[0:6])
-    bandits_players.append(players_list[6:12])
-    warriors_players.append(players_list[12:])
-
-   
 
 
 if __name__ == "__main__":
