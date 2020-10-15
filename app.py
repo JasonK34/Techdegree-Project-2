436e08bc08045d46cc086f610be96676833638e6
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-10-15 11:12:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,20 @@
 from constants import TEAMS
 from constants import PLAYERS
 import copy
+import random
 
 
-
-#my copies of the original files
 players_copy = copy.deepcopy(PLAYERS)
 teams_copy = copy.deepcopy(TEAMS)
 
-#new list of all players
 players_list = []
 guardian_list = []
 
-#each team's list of players
 panthers_players = []
 bandits_players = []
 warriors_players = []
 
 print("\nWelcome to Basketball Stats!\n")
-
 
 def clean_data():
     
@@ -34,7 +30,6 @@ def clean_data():
                 player["guardians"] = guardians
                 for guardian in guardians:
                     guardian_list.append(guardian)
-                    #print (guardian)
 
             if key == "height":
                 height = int(value.split(" ")[0])
@@ -46,17 +41,33 @@ def clean_data():
                 if "experience" == "NO":
                     player["experience"] = False
 
-    print(players_copy)
-
-def balance_teams():
+def balance_teams(players_copy):
     
+    print (players_copy)
+    panthers_players = random.sample(players_copy, 6)
+    print('\n', panthers_players)
+    players_copy = [x for x in players_copy if x not in panthers_players]
+    
+    bandits_players = random.sample(players_copy, 6)
+    print('\n', bandits_players)
+    players_copy = [x for x in players_copy if x not in bandits_players]
+
+    warriors_players = random.sample(players_copy, 6)
+    print('\n', warriors_players)
+    players_copy = [x for x in players_copy if x not in warriors_players]
+
+    # each team is assigned 6 players and they show up when printed here, but when the stat_tool runs these lists are empty
+    
+def stat_tool():
+
     initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n\n ---> "))
     while initial_entry == 1:
         select_team = int(input("\nSelect option below (enter numer):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) ALL Players\n (5) All Guardians\n\n ---> "))
         if select_team == 1:
-            print("\nPANTHERS:")
-            for player in players_copy[0:6]:
-                for key, value in player.items():
+            print("\nPANTHERS: \n")
+            print(panthers_players)  #this is just here to see if it prints the list, but it is empty and I cannot figure out why; it happens with all 3 teams
+            for panther in panthers_players:             
+                for key, value in panther.items():
                     if key == "name":
                         print(value)
                     if key == "height":
@@ -67,9 +78,10 @@ def balance_teams():
                         print(" * Experience: ", value)
 
         if select_team == 2:
-            print("\nBANDITS: ")
-            for player in players_copy[6:12]:
-                for key, value in player.items():
+            print("\nBANDITS: \n")
+            print(bandits_players)
+            for bandit in bandits_players:
+                for key, value in bandit.items():
                     if key == "name":
                         print(value)
                     if key == "height":
@@ -79,9 +91,10 @@ def balance_teams():
                     if key == "experience":
                         print(" * Experience: ", value)    
         if select_team == 3:
-            print("\nWARRIORS: ")
-            for player in players_copy[12:]:
-                for key, value in player.items():
+            print("\nWARRIORS: \n")
+            print(warriors_players)
+            for warrior in warriors_players:
+                for key, value in warrior.items():
                     if key == "name":
                         print(value)
                     if key == "height":
@@ -100,11 +113,13 @@ def balance_teams():
             print("\nGUARDIANS: \n")
             for guardian in guardian_list:
                 print(guardian)    
-
-
+                                 
+                                    
 if __name__ == "__main__":
     clean_data()
-    balance_teams()
+    balance_teams(players_copy)
+    stat_tool()
+
     
 
 
