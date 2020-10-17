eef7bf17cd9a69ac08ed6474bc96ec56305d0e7e
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-10-16 20:48:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,9 @@ def balance_teams():
     
 def stat_tool():
 
-    initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n\n ---> "))
+    initial_entry = int(input("\nPlease enter '1' for STATS, and '2' to QUIT: \n\n ---> "))
     while initial_entry == 1:
-        select_team = int(input("\nSelect option below (enter number):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) All Guardians\n (5) EXIT\n\n ---> "))
+        select_team = int(input("\nSelect option below (enter number):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) All Guardians\n (5) QUIT PROGRAM\n\n ---> "))
         if select_team == 1:
             print("\nPANTHERS: \n")
             print("Number of players: ", len(panthers_players))
