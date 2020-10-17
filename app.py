e21451630f1fb6616138d158947c768e8740b25d
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-10-16 20:24:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -43,76 +43,47 @@ def clean_data():
 
 def balance_teams():
     
+    global players_list
     global players_copy
     global panthers_players
     global bandits_players
     global warriors_players
 
-    panthers_players = random.sample(players_copy, 6)
-    players_copy = [x for x in players_copy if x not in panthers_players]
+    panthers_players = random.sample(players_list, 6)
+    players_list = [x for x in players_list if x not in panthers_players]
     
-    bandits_players = random.sample(players_copy, 6)
-    players_copy = [x for x in players_copy if x not in bandits_players]
+    bandits_players = random.sample(players_list, 6)
+    players_list = [x for x in players_list if x not in bandits_players]
 
-    warriors_players = random.sample(players_copy, 6)
-    players_copy = [x for x in players_copy if x not in warriors_players]
+    warriors_players = random.sample(players_list, 6)
+    players_list = [x for x in players_list if x not in warriors_players]
 
     
 def stat_tool():
 
     initial_entry = int(input("\nPlease enter '1' for stats, and '2' to exit: \n\n ---> "))
     while initial_entry == 1:
-        select_team = int(input("\nSelect option below (enter number):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) ALL Players\n (5) All Guardians\n (6) EXIT\n\n ---> "))
+        select_team = int(input("\nSelect option below (enter number):\n (1) PANTHERS roster\n (2) BANDITS roster\n (3) WARRIORS roster \n (4) All Guardians\n (5) EXIT\n\n ---> "))
         if select_team == 1:
             print("\nPANTHERS: \n")
-            for panther in panthers_players:             
-                for key, value in panther.items():
-                    if key == "name":
-                        print(value)
-                    if key == "height":
-                        print(" * Height: ", value, "inches")
-                    if key == "guardians":
-                        print(" * Guardian(s): ", ','.join(value))
-                    if key == "experience":
-                        print(" * Experience: ", value)
-
+            print("Number of players: ", len(panthers_players))
+            print(*panthers_players, sep=", ")
+ 
         if select_team == 2:
             print("\nBANDITS: \n")
-            print(bandits_players)
-            for bandit in bandits_players:
-                for key, value in bandit.items():
-                    if key == "name":
-                        print(value)
-                    if key == "height":
-                        print(" * Height: ", value, "inches")
-                    if key == "guardians":
-                        print(" * Guardian(s): ", ','.join(value))
-                    if key == "experience":
-                        print(" * Experience: ", value)    
+            print("Number of players: ", len(bandits_players))
+            print(*bandits_players, sep=", ")
+            
         if select_team == 3:
             print("\nWARRIORS: \n")
-            print(warriors_players)
-            for warrior in warriors_players:
-                for key, value in warrior.items():
-                    if key == "name":
-                        print(value)
-                    if key == "height":
-                        print(" * Height: ", value, "inches") 
-                    if key == "guardians":
-                        print(" * Guardian(s): ", ','.join(value))
-                    if key == "experience":
-                        print(" * Experience: ", value)   
-                        
+            print("Number of players: ", len(warriors_players))            
+            print(*warriors_players, sep=", ")  
+                                       
         if select_team == 4:
-            print("ALL players: \n")
-            for player in players_list:
-                print(player)
-                
-        if select_team == 5:
             print("\nGUARDIANS: \n")
             for guardian in guardian_list:
                 print(guardian)
-        if select_team == 6:
+        if select_team == 5:
             print("\nGoodbye\n")
             break    
                                  
@@ -125,5 +96,4 @@ if __name__ == "__main__":
     stat_tool()
 
     
-
 
